9d000545c8e4b1abd1998db1aa2af480937ff89a
KalilDev <kalilgamer0@gmail.com>
2021-11-08 15:18:30 -0300

    libmonet: Finishing touches

--- working_tree/theme_generator/bin/example.js
+++ working_tree/theme_generator/bin/example.js
@@ -4,4 +4,6 @@ const theme_generator = require('../lib/index.js');
 //console.log(libmonet)
 //console.log(theme_generator)
 let adapter = theme_generator.ThemeAdapter.fromColor('#deadbe', true)
-console.log(adapter.dark)
+adapter.save()
+let dspexporter = new theme_generator.ComposeExporter(adapter, 'deadbeef', (blob, a, _) => console.log(blob))
+console.log(dspexporter.generate())
