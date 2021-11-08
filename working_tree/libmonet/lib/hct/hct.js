68981a44f6adc757112b93fb31aa186872adb80e
KalilDev <kalilgamer0@gmail.com>
2021-11-08 14:16:39 -0300

    all: convert to node packages

--- working_tree/libmonet/lib/hct/hct.js
+++ working_tree/libmonet/lib/hct/hct.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const utils = require('../utils/utils.js');
-const cam16 = require('../hct/cam16.js');
+const cam16 = require('./cam16.js');
 const color_utils = require('../utils/color_utils.js');
 
 module.exports = {
